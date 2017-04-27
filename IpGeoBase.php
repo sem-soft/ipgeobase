cfd8549c58538c6e7b5220541fe863fa3a26ec14
S.E.M <samosnov.sem@gmail.com>
2017-04-27 19:33:26 +0300

    Инициализация

---
+++ IpGeoBase.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * @author Самсонов Владимир <samsonov.sem@gmail.com>
+ * @copyright Copyright &copy; S.E.M. 2017-
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+
+namespace sem\ipgeobase;
+
+use Yii;
+use yii\base\Component;
+
+/**
+ * Компонент реализует логику взаимодействиея с сервисом @link http://ipgeobase.ru/Help.html
+ * И работает как с локальной базой сервиса, которая находится в свободном доспуте, так и с сервисом напрямую
+ */
+class IpGeoBase extends Component
+{
+
+    /**
+     * Количество секунд для таймаута при работе с сервисом напрямую
+     * @var integer
+     */
+    public $serviceTimeout = 5;
+    
+    /**
+     * Возвращает IP-адрес клиентского запроса
+     * 
+     * @return string|null
+     */
+    public function getUserIp()
+    {
+        $userIp = null;
+        
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            
+            $userIp = $_SERVER['HTTP_CLIENT_IP'];
+            
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            
+            $userIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            
+        } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+            
+            $userIp = $_SERVER['REMOTE_ADDR'];
+            
+        }
+        
+        return $userIp;
+    }
+    
+    /**
+     * Возвращает геолокационные данные по IP-адресу.
+     * Если Ip-адрес не указан,
+     * то будет произведена попытка определения текущего IP-адреса пользователя
+     * @param string $ip IP-адрес, для котрого определяется GEO-информация
+     * @return \stdClass|null
+     */
+    public function getGeo($ip = null)
+    {
+	$geo = null;
+	
+	if (is_null($ip)) {
+	    $ip = $this->getUserIp();
+	}
+	
+	if ($ip) {
+	    
+	    if (!$geo = $this->byDb($ip)) {
+		$geo = $this->byCUrl($ip);
+	    }
+	    
+	}
+	
+	return $geo;
+    }
+    
+    /**
+     * Определение геолокации путем запроса из локальной БД
+     * @see http://faniska.ru/php-kusochki/import-bazy-ipgeobase-v-lokalnuyu-bazu-dannyx-i-dalnejshee-ispolzovanie.html
+     * @param string $ip IP-адрес, для котрого определяется GEO-информация
+     * @return \stdClass|null
+     */
+    protected function byDb($ip)
+    {
+	
+	try {
+	    
+	    $query = Yii::$app->db->createCommand(
+		"SELECT
+		    `b`.`country`,
+		    `c`.`city`,
+		    `c`.`region`,
+		    `c`.`district`,
+		    `c`.`lat`,
+		    `c`.`lng`
+		FROM
+		    {{%geo__base}} `b`
+		    LEFT JOIN
+		    {{%geo__cities}} `c` ON (`b`.`city_id` = `c`.`city_id`)
+		WHERE
+		    `b`.`long_ip1`<= :longIp AND `b`.`long_ip2`>= :longIp
+		LIMIT 1",[
+		    ':longIp'	=>  ip2long($ip)
+		]
+	    );
+	    
+	    $geo = null;
+	    
+	    if ($result = $query->queryOne()) {
+		
+		$geo = (object) [
+		    'country'   =>  $result['country'],
+		    'city'      =>  $result['city'],
+		    'region'    =>  $result['region'],
+		    'district'  =>  $result['district'],
+		    'lat'       =>  (float) $result['lat'],
+		    'lng'       =>  (float) $result['lng'],
+		];
+		
+	    }
+	    
+	} catch (\Exception $exc) {
+	   $geo = null; 
+	}
+	
+	return $geo;
+    }
+    
+    /**
+     * Определение геолокации путем непосредствееного взаимдействия с сервисом
+     * @param string $ip IP-адрес, для котрого определяется GEO-информация
+     * @return \stdClass|null
+     */
+    protected function byCUrl($ip)
+    {
+	$serviceUrl = 'http://ipgeobase.ru:7020/geo?ip=' . urlencode($ip);
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $serviceUrl);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, $this->serviceTimeout);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->serviceTimeout);
+
+	if ($xmlData = curl_exec($ch)) {
+	    
+	    $xml = new \SimpleXMLElement($xmlData);
+	    if ($xml->ip && !isset($xml->ip->message)) {
+		
+		return (object) [
+		    'country'   =>  (string)$xml->ip->country,
+		    'city'      =>  (string)$xml->ip->city,
+		    'region'    =>  (string)$xml->ip->region,
+		    'district'  =>  (string)$xml->ip->district,
+		    'lat'       =>  (float)$xml->ip->lat,
+		    'lng'       =>  (float)$xml->ip->lng,
+		];
+		
+	    }
+	    
+	}
+	
+	return null;
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Yii2 component geolocation http://ipgeobase.ru/ service
+## Install by composer
+composer require sem-soft/yii2-ipgeobase
+## Or add this code into require section of your composer.json and then call composer update in console
+"sem-soft/yii2-ipgeobase": "*"
+## Usage
+In configuration file do
