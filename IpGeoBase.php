d5b1c298bf7551f1a52a392e11c27ff77c5c2640
S.E.M <samosnov.sem@gmail.com>
2017-06-30 12:39:11 +0300

    PSR-2 форматирование

--- IpGeoBase.php
+++ IpGeoBase.php
@@ -4,7 +4,6 @@
  * @copyright Copyright &copy; S.E.M. 2017-
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
-
 namespace sem\ipgeobase;
 
 use Yii;
@@ -24,13 +23,12 @@ class IpGeoBase extends Component
      * @var integer
      */
     public $serviceTimeout = 5;
-    
+
     /**
      * Основной поисковый SQL-запроса
      * @var \yii\db\Query
      */
     protected $_query;
-
 
     /**
      * Возвращает IP-адрес клиентского запроса
@@ -40,24 +38,21 @@ class IpGeoBase extends Component
     public function getUserIp()
     {
         $userIp = null;
-        
+
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            
+
             $userIp = $_SERVER['HTTP_CLIENT_IP'];
-            
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            
+
             $userIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            
         } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
-            
+
             $userIp = $_SERVER['REMOTE_ADDR'];
-            
         }
-        
+
         return $userIp;
     }
-    
+
     /**
      * Возвращает геолокационные данные по IP-адресу.
      * Если Ip-адрес не указан,
@@ -67,23 +62,22 @@ class IpGeoBase extends Component
      */
     public function getGeo($ip = null)
     {
-	$geo = null;
-	
-	if (is_null($ip)) {
-	    $ip = $this->getUserIp();
-	}
-	
-	if ($ip) {
-	    
-	    if (!$geo = $this->byDb($ip)) {
-		$geo = $this->byCUrl($ip);
-	    }
-	    
-	}
-	
-	return $geo;
+        $geo = null;
+
+        if (is_null($ip)) {
+            $ip = $this->getUserIp();
+        }
+
+        if ($ip) {
+
+            if (!$geo = $this->byDb($ip)) {
+                $geo = $this->byCUrl($ip);
+            }
+        }
+
+        return $geo;
     }
-    
+
     /**
      * Возвращает Гео-информацию по названию города
      * @param string $city
@@ -91,36 +85,34 @@ class IpGeoBase extends Component
      */
     public function getCityInfo($city)
     {
-	try {
+        try {
 
-	    $query = $this->getQuery();
-	    $query->where('[[c]].[[city]] = :city');
-	    $query->params = [
-		':city'	=>  $city
-	    ];
-	    
-	    $info = null;
-	    
-	    if ($result = $query->one()) {
-		$info = (object) [
-		    'country'   =>  $result['country'],
-		    'city'      =>  $result['city'],
-		    'region'    =>  $result['region'],
-		    'district'  =>  $result['district'],
-		    'lat'       =>  (float) $result['lat'],
-		    'lng'       =>  (float) $result['lng'],
-		];
-	    }
-	    
-	} catch (\Exception $exc) {
-	    
-	    $info = null;
-	    
-	}
-	
-	return $info;
+            $query = $this->getQuery();
+            $query->where('[[c]].[[city]] = :city');
+            $query->params = [
+                ':city' => $city
+            ];
+
+            $info = null;
+
+            if ($result = $query->one()) {
+                $info = (object) [
+                        'country' => $result['country'],
+                        'city' => $result['city'],
+                        'region' => $result['region'],
+                        'district' => $result['district'],
+                        'lat' => (float) $result['lat'],
+                        'lng' => (float) $result['lng'],
+                ];
+            }
+        } catch (\Exception $exc) {
+
+            $info = null;
+        }
+
+        return $info;
     }
-    
+
     /**
      * Определение геолокации путем запроса из локальной БД
      * @see http://faniska.ru/php-kusochki/import-bazy-ipgeobase-v-lokalnuyu-bazu-dannyx-i-dalnejshee-ispolzovanie.html
@@ -129,37 +121,35 @@ class IpGeoBase extends Component
      */
     protected function byDb($ip)
     {
-	
-	try {
-	    
-	    $query = $this->getQuery();
-	    $query->where('[[b]].[[long_ip1]] <= :longIp AND [[b]].[[long_ip2]] >= :longIp');
-	    $query->params = [
-		':longIp'	=>  ip2long($ip)
-	    ];
-	    
-	    $geo = null;
-	    
-	    if ($result = $query->one()) {
-		
-		$geo = (object) [
-		    'country'   =>  $result['country'],
-		    'city'      =>  $result['city'],
-		    'region'    =>  $result['region'],
-		    'district'  =>  $result['district'],
-		    'lat'       =>  (float) $result['lat'],
-		    'lng'       =>  (float) $result['lng'],
-		];
-		
-	    }
-	    
-	} catch (\Exception $exc) {
-	   $geo = null; 
-	}
-	
-	return $geo;
+
+        try {
+
+            $query = $this->getQuery();
+            $query->where('[[b]].[[long_ip1]] <= :longIp AND [[b]].[[long_ip2]] >= :longIp');
+            $query->params = [
+                ':longIp' => ip2long($ip)
+            ];
+
+            $geo = null;
+
+            if ($result = $query->one()) {
+
+                $geo = (object) [
+                        'country' => $result['country'],
+                        'city' => $result['city'],
+                        'region' => $result['region'],
+                        'district' => $result['district'],
+                        'lat' => (float) $result['lat'],
+                        'lng' => (float) $result['lng'],
+                ];
+            }
+        } catch (\Exception $exc) {
+            $geo = null;
+        }
+
+        return $geo;
     }
-    
+
     /**
      * Определение геолокации путем непосредствееного взаимдействия с сервисом
      * @param string $ip IP-адрес, для котрого определяется GEO-информация
@@ -167,62 +157,58 @@ class IpGeoBase extends Component
      */
     protected function byCUrl($ip)
     {
-	$serviceUrl = 'http://ipgeobase.ru:7020/geo?ip=' . urlencode($ip);
+        $serviceUrl = 'http://ipgeobase.ru:7020/geo?ip=' . urlencode($ip);
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $serviceUrl);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_TIMEOUT, $this->serviceTimeout);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->serviceTimeout);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $serviceUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->serviceTimeout);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->serviceTimeout);
 
-	if ($xmlData = curl_exec($ch)) {
-	    
-	    $xml = new \SimpleXMLElement($xmlData);
-	    if ($xml->ip && !isset($xml->ip->message)) {
-		
-		return (object) [
-		    'country'   =>  (string)$xml->ip->country,
-		    'city'      =>  (string)$xml->ip->city,
-		    'region'    =>  (string)$xml->ip->region,
-		    'district'  =>  (string)$xml->ip->district,
-		    'lat'       =>  (float)$xml->ip->lat,
-		    'lng'       =>  (float)$xml->ip->lng,
-		];
-		
-	    }
-	    
-	}
-	
-	return null;
+        if ($xmlData = curl_exec($ch)) {
+
+            $xml = new \SimpleXMLElement($xmlData);
+            if ($xml->ip && !isset($xml->ip->message)) {
+
+                return (object) [
+                        'country' => (string) $xml->ip->country,
+                        'city' => (string) $xml->ip->city,
+                        'region' => (string) $xml->ip->region,
+                        'district' => (string) $xml->ip->district,
+                        'lat' => (float) $xml->ip->lat,
+                        'lng' => (float) $xml->ip->lng,
+                ];
+            }
+        }
+
+        return null;
     }
-    
+
     /**
      * Подготавливает и возвращает основной запрос
      * @return \yii\db\Query
      */
     protected function getQuery()
     {
-	
-	if (is_null($this->_query)) {
-	    
-	    $this->_query = (new \yii\db\Query)->select([
-		'[[b]].[[country]]',
-		'[[c]].[[city]]',
-		'[[c]].[[region]]',
-		'[[c]].[[district]]',
-		'[[c]].[[lat]]',
-		'[[c]].[[lng]]'
-	    ])->
-	    from('{{%geo__base}} b')->
-	    leftJoin([
-		'{{%geo__cities}} c'
-	    ], '[[b]].[[city_id]] = [[c]].[[city_id]]')->
-	    limit(1);
-	    
-	}
-	
-	return $this->_query;
+
+        if (is_null($this->_query)) {
+
+            $this->_query = (new \yii\db\Query)->select([
+                    '[[b]].[[country]]',
+                    '[[c]].[[city]]',
+                    '[[c]].[[region]]',
+                    '[[c]].[[district]]',
+                    '[[c]].[[lat]]',
+                    '[[c]].[[lng]]'
+                ])->
+                from('{{%geo__base}} b')->
+                leftJoin([
+                    '{{%geo__cities}} c'
+                    ], '[[b]].[[city_id]] = [[c]].[[city_id]]')->
+                limit(1);
+        }
+
+        return $this->_query;
     }
-    
 }
--- migrations/m170228_151006_create_ipgeobase_structure.php
+++ migrations/m170228_151006_create_ipgeobase_structure.php
@@ -4,84 +4,85 @@ use yii\db\Migration;
 
 class m170228_151006_create_ipgeobase_structure extends Migration
 {
+
     public function up()
     {
         $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=MyISAM';
         $this->createTable('{{%geo__base}}', [
-            'long_ip1'      =>	$this->bigInteger(20)->notNull(),
-	    'long_ip2'	    =>  $this->bigInteger(20)->notNull(),
-	    'ip1'	    =>	$this->string(16)->notNull(),
-	    'ip2'	    =>	$this->string(16)->notNull(),
-	    'country'	    =>	$this->string(2)->notNull(),
-	    'city_id'	    =>	$this->string(10)->notNull(),
-        ], $tableOptions);
-	$this->createIndex('idx_geo__base_long_ips', '{{%geo__base}}', [
-	    'long_ip1',
-	    'long_ip2'
-	]);
-	
-	$this->createTable('{{%geo__cities}}', [
-	    'city_id'	=>  $this->primaryKey(),
-	    'city'	=> $this->string(128)->notNull(),
-	    'region'	=> $this->string(128)->notNull(),
-	    'district'	=> $this->string(128)->notNull(),
-	    'lat'	=> $this->float()->notNull(),
-	    'lng'	=>  $this->float()->notNull(),
-	], $tableOptions);
-	
-	$this->importCities();
-	$this->importBase();
+            'long_ip1' => $this->bigInteger(20)->notNull(),
+            'long_ip2' => $this->bigInteger(20)->notNull(),
+            'ip1' => $this->string(16)->notNull(),
+            'ip2' => $this->string(16)->notNull(),
+            'country' => $this->string(2)->notNull(),
+            'city_id' => $this->string(10)->notNull(),
+            ], $tableOptions);
+        $this->createIndex('idx_geo__base_long_ips', '{{%geo__base}}', [
+            'long_ip1',
+            'long_ip2'
+        ]);
+
+        $this->createTable('{{%geo__cities}}', [
+            'city_id' => $this->primaryKey(),
+            'city' => $this->string(128)->notNull(),
+            'region' => $this->string(128)->notNull(),
+            'district' => $this->string(128)->notNull(),
+            'lat' => $this->float()->notNull(),
+            'lng' => $this->float()->notNull(),
+            ], $tableOptions);
+
+        $this->importCities();
+        $this->importBase();
     }
 
     public function down()
     {
-	$this->dropTable('{{%geo__cities}}');
-	$this->dropTable('{{%geo__base}}');
+        $this->dropTable('{{%geo__cities}}');
+        $this->dropTable('{{%geo__base}}');
     }
-    
+
     protected function importCities()
     {
-	$this->execute("TRUNCATE TABLE {{%geo__cities}}");
-	if ($file = file(__DIR__ . '/data/geo/cities.txt')) {
-	    $pattern = '#(\d+)\s+(.*?)\t+(.*?)\t+(.*?)\t+(.*?)\s+(.*)#';
-	    foreach ($file as $row) {
-		$nRow = iconv('windows-1251', 'utf-8', $row);
-		if (preg_match($pattern, $nRow, $out)) {
-		    $this->insert('{{%geo__cities}}', [
-			    'city_id'	=>  $out[1],
-			    'city'	=>  $out[2],
-			    'region'	=>  $out[3],
-			    'district'	=>  $out[4],
-			    'lat'	=>  $out[5],
-			    'lng'	=>  $out[6],
-		    ]);
-		}
-	    }
-	    return true;
-	}
-	return false;
+        $this->execute("TRUNCATE TABLE {{%geo__cities}}");
+        if ($file = file(__DIR__ . '/data/geo/cities.txt')) {
+            $pattern = '#(\d+)\s+(.*?)\t+(.*?)\t+(.*?)\t+(.*?)\s+(.*)#';
+            foreach ($file as $row) {
+                $nRow = iconv('windows-1251', 'utf-8', $row);
+                if (preg_match($pattern, $nRow, $out)) {
+                    $this->insert('{{%geo__cities}}', [
+                        'city_id' => $out[1],
+                        'city' => $out[2],
+                        'region' => $out[3],
+                        'district' => $out[4],
+                        'lat' => $out[5],
+                        'lng' => $out[6],
+                    ]);
+                }
+            }
+            return true;
+        }
+        return false;
     }
-    
+
     protected function importBase()
     {
-	$this->execute("TRUNCATE TABLE {{%geo__base}}");
-	if ($file = file(__DIR__ . '/data/geo/cidr_optim.txt')) {
-	    $pattern = '#(\d+)\s+(\d+)\s+(\d+\.\d+\.\d+\.\d+)\s+-\s+(\d+\.\d+\.\d+\.\d+)\s+(\w+)\s+(\d+|-)#';
-	    foreach ($file as $row) {
-		$nRow = iconv('windows-1251', 'utf-8', $row);
-		if (preg_match($pattern, $nRow, $out)) {
-		    $this->insert('{{%geo__base}}', [
-			'long_ip1'  =>  $out[1],
-			'long_ip2'  =>  $out[2],
-			'ip1'	    =>  $out[3],
-			'ip2'	    =>  $out[4],
-			'country'   =>  $out[5],
-			'city_id'   =>  $out[6],
-		    ]);
-		}
-	    }
-	    return true;
-	}
-	return false;
+        $this->execute("TRUNCATE TABLE {{%geo__base}}");
+        if ($file = file(__DIR__ . '/data/geo/cidr_optim.txt')) {
+            $pattern = '#(\d+)\s+(\d+)\s+(\d+\.\d+\.\d+\.\d+)\s+-\s+(\d+\.\d+\.\d+\.\d+)\s+(\w+)\s+(\d+|-)#';
+            foreach ($file as $row) {
+                $nRow = iconv('windows-1251', 'utf-8', $row);
+                if (preg_match($pattern, $nRow, $out)) {
+                    $this->insert('{{%geo__base}}', [
+                        'long_ip1' => $out[1],
+                        'long_ip2' => $out[2],
+                        'ip1' => $out[3],
+                        'ip2' => $out[4],
+                        'country' => $out[5],
+                        'city_id' => $out[6],
+                    ]);
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
